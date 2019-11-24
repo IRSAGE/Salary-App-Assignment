b3223c48507f5db262bd143ad56737b02cfe131e
IRSAGE <sibo.egide98@gmail.com>
2019-11-24 22:01:01 +0200

    adding salary app assignment folder

---
+++ Salary App Assignment/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Salary_App_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
